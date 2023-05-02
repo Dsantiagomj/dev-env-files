7a726837e0aef845235b3a000382e2bc64a5b1b1
Daniel Santiago <dsantiagomj@Daniels-MacBook-Pro-M1.local>
2023-05-01 23:23:28 -0500

    dev env files'

---
+++ nvim/init.lua
@@ -0,0 +1,20 @@
+-- plugins setup
+require("dsantiagomj.plugins-setup")
+-- core configuration
+require("dsantiagomj.core.options")
+require("dsantiagomj.core.keymaps")
+require("dsantiagomj.core.colorscheme")
+-- plugins
+require("dsantiagomj.plugins.comment")
+require("dsantiagomj.plugins.nvim-tree")
+require("dsantiagomj.plugins.lualine")
+require("dsantiagomj.plugins.telescope")
+require("dsantiagomj.plugins.nvim-cmp")
+require("dsantiagomj.plugins.autopairs")
+require("dsantiagomj.plugins.treesitter")
+require("dsantiagomj.plugins.gitsigns")
+-- lsp configuration
+require("dsantiagomj.plugins.lsp.mason")
+require("dsantiagomj.plugins.lsp.lspsaga")
+require("dsantiagomj.plugins.lsp.lspconfig")
+require("dsantiagomj.plugins.lsp.null-ls")
